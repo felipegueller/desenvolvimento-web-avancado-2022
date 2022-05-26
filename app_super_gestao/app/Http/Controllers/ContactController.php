604faf2b4fbd056951619d6640e772891aade37c
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2022-05-25 21:45:40 -0300

    feat: creating my first controllers

---
+++ app_super_gestao/app/Http/Controllers/ContactController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function Contact() {
+        echo 'If you want to tell us something, contatc us!';
+    }
+}
